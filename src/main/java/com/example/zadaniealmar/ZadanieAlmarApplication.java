509404973058b5e443e8db439c674359501ae942
Lukasz Debski <lukasz.debski@almar.pl>
2022-06-06 22:49:28 +0200

    Test

---
+++ src/main/java/com/example/zadaniealmar/ZadanieAlmarApplication.java
@@ -0,0 +1,14 @@
+package com.example.zadaniealmar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieAlmarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieAlmarApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/zadaniealmar/ZadanieAlmarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadaniealmar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieAlmarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
